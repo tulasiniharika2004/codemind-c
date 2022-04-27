979fccd4f42382ba4bdb2e74c9f4dc370b950e05
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-04-27 14:06:26 +0530

    #include<stdio.h> int main() {     int n,sum=0,r;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         sum=sum*10+r;         n=n/10;     }     printf("%d",sum); }

--- Reverse_Integer.c
+++ Reverse_Integer.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,t,rev=0,r;
+    int n,sum=0,r;
     scanf("%d",&n);
     while(n!=0)
     {
         r=n%10;
-        rev=rev*10+r;
+        sum=sum*10+r;
         n=n/10;
     }
-    printf("%d",rev);
+    printf("%d",sum);
 }
