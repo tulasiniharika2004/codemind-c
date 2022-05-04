43146e36aeea748874c5d4cbc81d0a8e25953445
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-05-04 06:04:04 +0530

    #include<stdio.h> int main() {     int n,i,a[100],m;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             m=a[i];         }     }     printf("%d",m); }

---
+++ Last_even_number_in_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],m;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            m=a[i];
+        }
+    }
+    printf("%d",m);
+}
+    
+    
