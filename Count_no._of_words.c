5060a243fca19276056cbc63e6e5b19cb0da9700
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-06-27 21:55:04 +0530

    #include<stdio.h> int main() {     char s[200];     int c=0,i;     scanf("%[^ ]s",s);     for(i=0;s[i]!=NULL;i++)     {         if(s[i]==' '&& s[i+1]!=' ')         {             c++;         }     }     printf("%d ",c+1); }

---
+++ Count_no._of_words.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char s[200];
+    int c=0,i;
+    scanf("%[^
+]s",s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]==' '&& s[i+1]!=' ')
+        {
+            c++;
+        }
+    }
+    printf("%d
+",c+1);
+}
