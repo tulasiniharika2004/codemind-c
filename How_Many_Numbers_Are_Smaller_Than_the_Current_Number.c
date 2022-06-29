053299e822ff09d77be1fe22563dd08af4ff9fdf
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-06-29 20:43:22 +0530

    #include<stdio.h> int main() {     int n,i,j,c1=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c1=0;         for(j=0;j<n;j++)         {             if(i!=j)             {                 if(arr[i]>arr[j])                 c1++;                 else                 continue;             }         }         printf("%d ",c1);     } }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c1=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c1=0;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(arr[i]>arr[j])
+                c1++;
+                else
+                continue;
+            }
+        }
+        printf("%d ",c1);
+    }
+}
