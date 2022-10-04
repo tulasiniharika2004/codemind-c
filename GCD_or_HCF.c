8aae89d770ca5ea59f2f71975be4529e23fbc2cf
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-04 17:51:19 +0530

    #include<stdio.h> int main() { 	int x,y,i,hcf; 	scanf("%d%d",&x,&y); 	for(i=x;i>=1;i--) 	{ 		if(x%i==0 && y%i==0) 		{ 		 printf("%d",i); 		 break; 		} 	}
    }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,22 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,i,t;
-    scanf("%d%d",&n,&m);
-    if(n<m)
-    {
-        t=n;
-    }
-    else
-    {
-        t=m;
-    }
-    for(i=t;i>=1;i--)
-    {
-        if(n%i==0 && m%i==0)
-        {
-            printf("%d",i);
-            break;
-        }
-    }
+	int x,y,i,hcf;
+	scanf("%d%d",&x,&y);
+	for(i=x;i>=1;i--)
+	{
+		if(x%i==0 && y%i==0)
+		{
+		 printf("%d",i);
+		 break;
+		}
+	}
+    
 }
