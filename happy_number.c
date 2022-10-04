9939f0c3565fb37824c02cfc474c519130d00a2c
tulasiniharika2004 <97425801+tulasiniharika2004@users.noreply.github.com>
2022-10-04 14:28:36 +0530

    #include<stdio.h> int happy(int n) {     int temp,r,sum=0;     temp=n;     while(temp)     {         r=temp%10;         sum=sum+(r*r);         temp=temp/10;     }     if(sum>=10)     {         happy(sum);     }     else     {         if(sum==1 || sum==7)         {             return 1;         }         return 0;     } } int main() {     int n;     scanf("%d",&n);    if( happy(n))    {        printf("True");    }    else    {        printf("False");    }
    }

--- happy_number.c
+++ happy_number.c
@@ -1,26 +1,38 @@
 #include<stdio.h>
+int happy(int n)
+{
+    int temp,r,sum=0;
+    temp=n;
+    while(temp)
+    {
+        r=temp%10;
+        sum=sum+(r*r);
+        temp=temp/10;
+    }
+    if(sum>=10)
+    {
+        happy(sum);
+    }
+    else
+    {
+        if(sum==1 || sum==7)
+        {
+            return 1;
+        }
+        return 0;
+    }
+}
 int main()
 {
     int n;
     scanf("%d",&n);
-    int sum=0,r;
-    while(sum!=1 && sum!=4)
-    {
-        sum=0;
-        while(n!=0)
-        {
-            r=n%10;
-            sum+=(r*r);
-            n=n/10;
-        }
-        n=sum;
-    }
-    if(sum==1)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
+   if( happy(n))
+   {
+       printf("True");
+   }
+   else
+   {
+       printf("False");
+   }
+    
 }
